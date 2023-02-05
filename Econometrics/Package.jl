7c3d10ccc485d137e16a6c5a9c358db579e14af5
Walasse Mickael <walasse.tomaz@gmail.com>
2023-02-04 21:09:47 -0300

    Módulo Uso Externo

--- Econometrics/Package.jl
+++ Econometrics/Package.jl
@@ -1,120 +1,110 @@
-#----------------------------------------------------------------------------------------------------Configurações
+module 
+#--------------------------------------------------------------------------------------------------------Configurações
 using Pkg
 pkg"add DataFrames"
-#-------------------------------------------------------------------------------------------------Dados Utilizados
+#-----------------------------------------------------------------------------------------------------Dados Utilizados
 
 using DataFrames
 
 x = [2,2,2,3,3,3,4,4,4]
 y = [0.1,0.1,0.2,0.2,0.3,0.3,0.3,0.4,0.4]
 
-#-----------------------------------------------------------------------------------------------------------------
-# Média Aritimética
-#-----------------------------------------------------------------------------------------------------------------
+module Econometrics
 
-function ArithmeticMean( Values )
-    return sum( Values ) / length( Values )
-end
+    #-----------------------------------------------------------------------------------------------------------------
+    # Média Aritimética
+    #-----------------------------------------------------------------------------------------------------------------
 
-ArithmeticMean(x)
+    function ArithmeticMean( Values )
+        return sum( Values ) / length( Values )
+    end
 
-#-----------------------------------------------------------------------------------------------------------------
-# Média Ponderada
-#-----------------------------------------------------------------------------------------------------------------
+    #-----------------------------------------------------------------------------------------------------------------
+    # Média Ponderada
+    #-----------------------------------------------------------------------------------------------------------------
 
-function WeightedMean( Values , Weight )
-    WeightedValuesList = []
-    
-    if length( Values ) === length( Weight )
-        for ( index, value ) in enumerate( Values )
-            WeightedValues = (value * Weight[index])
-            pushfirst!( WeightedValuesList , WeightedValues)
+    function WeightedMean( Values , Weight )
+        WeightedValuesList = []
+        
+        if length( Values ) === length( Weight )
+            for ( index, value ) in enumerate( Values )
+                WeightedValues = (value * Weight[index])
+                pushfirst!( WeightedValuesList , WeightedValues)
+            end
+        else
+            println("Error")
         end
-    else
-        println("Error")
+        return sum( WeightedValuesList ) / sum( Weight )
     end
-    return sum( WeightedValuesList ) / sum( Weight )
-end
 
-WeightedMean(x, y)
+    #-----------------------------------------------------------------------------------------------------------------
+    # Média Geométrica (Índice de Fischer)
+    #-----------------------------------------------------------------------------------------------------------------
 
-#-----------------------------------------------------------------------------------------------------------------
-# Média Geométrica (Índice de Fischer)
-#-----------------------------------------------------------------------------------------------------------------
-
-function GeometricMean( Values )
-    return sum( Values ) ^ (1 // length( Values ))
-end
-
-GeometricMean(x)
-
-#-----------------------------------------------------------------------------------------------------------------
-# Mediana
-#-----------------------------------------------------------------------------------------------------------------
-
-function Median( Values )
-   if length( Values ) % 2 != 0 # Impar
-    sort( Values )
-    MedianPoint = (length( Values ) + 1) / 2
-    Value = Values[Int(MedianPoint)]
-   else # Par
-    sort( Values )
-    MedianPoint = length( Values ) / 2
-    Value = ArithmeticMean(Values[[Int(MedianPoint), Int(MedianPoint) + 1]])
-   end
-   return Value
-end
-
-Median(x)
-
-#-----------------------------------------------------------------------------------------------------------------
-# Moda Estatistica
-#-----------------------------------------------------------------------------------------------------------------
-
-function Mode( Values )
-    FrequencyTable = DataFrame("Values" => unique( x ))
-    FrequencyTable = insertcols(FrequencyTable, :Frequency => [count( i -> (i == Value), x) for Value in FrequencyTable.Values])
-    return FrequencyTable.Values[FrequencyTable.Frequency .== maximum(FrequencyTable.Frequency)]
-end
-
-Mode(x)
-
-#-----------------------------------------------------------------------------------------------------------------
-# Variância
-#-----------------------------------------------------------------------------------------------------------------
-
-function Variance( Values )
-    DistanceOfMean = []
-
-    for Value in Values
-        pushfirst!( DistanceOfMean ,(Value - ArithmeticMean( Values )) ^ 2)
+    function GeometricMean( Values )
+        return sum( Values ) ^ (1 // length( Values ))
     end
-    return sum(DistanceOfMean) / (length( Values ) - 1)
-end
 
-Variance(x)
+    #-----------------------------------------------------------------------------------------------------------------
+    # Mediana
+    #-----------------------------------------------------------------------------------------------------------------
 
-#-----------------------------------------------------------------------------------------------------------------
-# Desvio Médio Absoluto
-#-----------------------------------------------------------------------------------------------------------------
-
-function AbsoluteAverageDeviation( Values )
-    DistanceOfMean = []
-
-    for Value in Values
-        pushfirst!( DistanceOfMean , abs(Value - ArithmeticMean( Values )))
+    function Median( Values )
+    if length( Values ) % 2 != 0 # Impar
+        sort( Values )
+        MedianPoint = (length( Values ) + 1) / 2
+        Value = Values[Int(MedianPoint)]
+    else # Par
+        sort( Values )
+        MedianPoint = length( Values ) / 2
+        Value = ArithmeticMean(Values[[Int(MedianPoint), Int(MedianPoint) + 1]])
     end
-    return sum(DistanceOfMean) / length( Values )
+    return Value
+    end
+
+    #-----------------------------------------------------------------------------------------------------------------
+    # Moda Estatistica
+    #-----------------------------------------------------------------------------------------------------------------
+
+    function Mode( Values )
+        FrequencyTable = DataFrame("Values" => unique( x ))
+        FrequencyTable = insertcols(FrequencyTable, :Frequency => [count( i -> (i == Value), x) for Value in FrequencyTable.Values])
+        return FrequencyTable.Values[FrequencyTable.Frequency .== maximum(FrequencyTable.Frequency)]
+    end
+
+    #-----------------------------------------------------------------------------------------------------------------
+    # Variância
+    #-----------------------------------------------------------------------------------------------------------------
+
+    function Variance( Values )
+        DistanceOfMean = []
+
+        for Value in Values
+            pushfirst!( DistanceOfMean ,(Value - ArithmeticMean( Values )) ^ 2)
+        end
+        return sum(DistanceOfMean) / (length( Values ) - 1)
+    end
+
+    #-----------------------------------------------------------------------------------------------------------------
+    # Desvio Médio Absoluto
+    #-----------------------------------------------------------------------------------------------------------------
+
+    function AbsoluteAverageDeviation( Values )
+        DistanceOfMean = []
+
+        for Value in Values
+            pushfirst!( DistanceOfMean , abs(Value - ArithmeticMean( Values )))
+        end
+        return sum(DistanceOfMean) / length( Values )
+    end
+
+    #-----------------------------------------------------------------------------------------------------------------
+    # Desvio Padrão
+    #-----------------------------------------------------------------------------------------------------------------
+
+    function StanderdDeviation( Values )
+        return sqrt( Variance( Values ) )
+    end
+
+#---------------------------------------------------------------------------------------------------------------------    
 end
-
-AbsoluteAverageDeviation(x)
-
-#-----------------------------------------------------------------------------------------------------------------
-# Desvio Padrão
-#-----------------------------------------------------------------------------------------------------------------
-
-function StanderdDeviation( Values )
-    return sqrt( Variance( Values ) )
-end
-
-StanderdDeviation(x)
