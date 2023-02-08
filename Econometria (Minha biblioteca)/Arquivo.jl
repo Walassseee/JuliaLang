50ddfd26e0ccc826938698d172fc1d6510476327
Walasse Mickael <walasse.tomaz@gmail.com>
2023-02-08 20:18:18 -0300

    Montando biblioteca

---
+++ Econometria (Minha biblioteca)/Arquivo.jl
@@ -0,0 +1,32 @@
+using Markdown
+
+"""
+Biblioteca Econometria.
+
+O desenvolvimento desta biblioteca tem como intuíto a elaboração de
+ferramentas de análise econométrica para a linguagem de programação
+Julia Lang desenvolvida pelo MIT. 
+
+Este é um projeto individual e independente idealizado por um acadêmico
+de ciências econômicas e pré-mestrando de desenvolimento econômico e 
+economia aplicada da UFPR.
+
+Feito por: Walasse Mickael Frutuoso Tomaz.
+"""
+
+module econometria
+
+# Modulos utilizados
+using DataFrames
+
+"""
+
+function media() é a função que irá calcular a média aritimética dos vetores, caso numéricos,
+de todo o conjutos de dados. Caso o vetor for não numérico irá ignorar.
+
+"""
+  
+# Criando função
+
+
+end
